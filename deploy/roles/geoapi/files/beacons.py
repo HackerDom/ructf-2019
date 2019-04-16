b4f670cf24acf8175a738344e438bdee23e74867
Alexander Bersenev <bay@hackerdom.ru>
2019-04-17 02:04:20 +0500

    service deploy templates

--- deploy/roles/geoapi/files/beacons.py
+++ deploy/roles/geoapi/files/beacons.py
@@ -3,20 +3,13 @@
 import asyncio
 
 async def handle_echo(reader, writer):
-    data = await reader.read(100)
-    message = data.decode()
-    addr = writer.get_extra_info('peername')
-    print("Received %r from %r" % (message, addr))
-
-    print("Send: %r" % message)
-    writer.write(data)
+    writer.write(b"The GeoAPI service is not implemented yet")
     await writer.drain()
 
-    print("Close the client socket")
     writer.close()
 
 loop = asyncio.get_event_loop()
-coro = asyncio.start_server(handle_echo, "0.0.0.0", 31337, loop=loop)
+coro = asyncio.start_server(handle_echo, "0.0.0.0", 10002, loop=loop)
 server = loop.run_until_complete(coro)
 
 # Serve requests until Ctrl+C is pressed
@@ -26,7 +19,6 @@ try:
 except KeyboardInterrupt:
     pass
 
-# Close the server
 server.close()
 loop.run_until_complete(server.wait_closed())
 loop.close()
