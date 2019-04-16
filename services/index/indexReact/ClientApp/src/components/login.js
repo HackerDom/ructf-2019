776e6fe968de28dc6a2bad56e92161dbe518e78b
Дубровин Алексей <a.dubrovin@skbkontur.ru>
2019-04-16 22:26:13 +0500

    logout

--- services/index/indexReact/ClientApp/src/components/login.js
+++ services/index/indexReact/ClientApp/src/components/login.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Button, Col, Container, Form, FormGroup, Input, Label } from 'reactstrap';
 import './NavMenu.css';
-import { Redirect } from 'react-router-dom';
 
 export default class Login extends React.Component {
     constructor(props) {
@@ -9,7 +8,6 @@ export default class Login extends React.Component {
         this.state = {
             login: '',
             pwd: '',
-            loggedIn: false,
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -24,10 +22,8 @@ export default class Login extends React.Component {
                 body: form
             }).then(_ => {
                 props.history.push('/');
-                console.log("asd");
-                this.setState({ loggedIn: true });
             });
-        }
+        };
     }
 
     handleChange = async (event) => {
@@ -39,31 +35,29 @@ export default class Login extends React.Component {
     };
 
     render() {
-        const { login, pwd, loggedIn } = this.state;
-        return !loggedIn ? (
-            <Container>
-                <Col sm={12} md={{ size: 4, offset: 4 }}>
-                    <Form onSubmit={this.submitForm(this.props)} id="loginForm">
-                        <FormGroup row>
-                            <Label for="login" sm={3}>Login</Label>
-                            <Col sm={9}>
-                                <Input type="text" name="login" id="login" value={login} onChange={this.handleChange} />
-                            </Col>
-                        </FormGroup>
-                        <FormGroup row>
-                            <Label for="pwd" sm={3}>Password</Label>
-                            <Col sm={9}>
-                                <Input type="password" name="pwd" id="pwd" value={pwd} onChange={this.handleChange} />
-                            </Col>
-                        </FormGroup>
-                        <FormGroup check row>
-                            <Col sm={{ size: 20, offset: 3 }}>
-                                <Button>Submit</Button>
-                            </Col>
-                        </FormGroup>
-                    </Form>
-                </Col>
-            </Container>
-        ) : <Redirect to='/' />;
+        const { login, pwd } = this.state;
+        return <Container>
+            <Col sm={12} md={{ size: 4, offset: 4 }}>
+                <Form onSubmit={this.submitForm(this.props)} id="loginForm">
+                    <FormGroup row>
+                        <Label for="login" sm={3}>Login</Label>
+                        <Col sm={9}>
+                            <Input type="text" name="login" id="login" value={login} onChange={this.handleChange} />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                        <Label for="pwd" sm={3}>Password</Label>
+                        <Col sm={9}>
+                            <Input type="password" name="pwd" id="pwd" value={pwd} onChange={this.handleChange} />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup check row>
+                        <Col sm={{ size: 20, offset: 3 }}>
+                            <Button>Submit</Button>
+                        </Col>
+                    </FormGroup>
+                </Form>
+            </Col>
+        </Container>;
     }
 }
