66a4597936b0a90a94d8c8e8076d38cfe418e0f6
Дубровин Алексей <a.dubrovin@skbkontur.ru>
2019-04-20 21:25:50 +0500

    remove test))0)

--- services/index/indexReact/Program.cs
+++ services/index/indexReact/Program.cs
@@ -5,10 +5,10 @@ namespace indexReact
 {
     public class Program
     {
-//        public static void Main(string[] args)
-//        {
-//            CreateWebHostBuilder(args).Build().Run();
-//        }
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
