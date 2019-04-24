8c2fa11b311ed85e6522a5ea656908e3feb1e0b7
Daniil Sharko <integrex@yandex.ru>
2019-04-25 00:33:04 +0500

    reverse symlinks

--- checkers/brainhugger/bh_translator.py
+++ checkers/brainhugger/bh_translator.py
@@ -1,1 +1,21 @@
-../../sploits/brainhugger/bh_translator.py
+import pickle
+import zlib
+
+
+with open("table", "rb") as table_file:
+    table = pickle.loads(zlib.decompress(table_file.read()))
+
+
+def translate(source: bytes):
+    result = []
+    lastc = 0
+    for c in source:
+        a = table[lastc][c]
+        b = table[0][c]
+        if len(a) <= len(b):
+            result.append(a)
+        else:
+            result.append(">" + b)
+        result.append('.')
+        lastc = c
+    return ''.join(result)
