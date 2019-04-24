de89313daa991874bbbc8db83c8211cd6f90b6eb
Alexey <a.dubrovin@skbkontur.ru>
2019-04-25 01:09:27 +0500

    second checker lol

--- checkers/index/checker.py
+++ checkers/index/checker.py
@@ -3,9 +3,10 @@ from infrastructure.actions import Checker
 from infrastructure.verdict import Verdict
 from utils.http import build_session
 from utils.randomizer import Randomizer
+from words import get_text
 from zip_utils import create_zip, create_flag_zip
 
-Checker.INFO = "1"
+Checker.INFO = "3:1"
 
 
 @Checker.define_check
@@ -26,6 +27,10 @@ def check_service(host: str) -> Verdict:
             resp = api.search_file(file_in_zip)
             if file_in_zip not in resp.text:
                 return Verdict.MUMBLE("Can't find file from zip", "Can't find file from zip")
+
+            resp = api.create_note(get_text(), True)
+            if resp.status_code != 201:
+                return Verdict.MUMBLE("Can't create note", "Can't create note")
 
             return Verdict.OK()
     except Exception as e:
@@ -73,6 +78,49 @@ def get_zip_flag(host: str, flag_id: str, flag: str) -> Verdict:
         return Verdict.DOWN("Can't connect to service", str(e))
 
 
+@Checker.define_put(vuln_num=2)
+def put_zip(host: str, flag_id: str, flag: str) -> Verdict:
+    try:
+        with build_session() as session:
+            api = Api(host, session)
+
+            user = Randomizer.user()
+            resp = api.register_user(user)
+            if resp.status_code != 201:
+                return Verdict.MUMBLE("Can't register user", "Can't register user")
+
+            resp = api.create_note(flag, False)
+            if resp.status_code != 201:
+                return Verdict.MUMBLE("Can't create note", "Can't create note")
+
+            return Verdict.OK(f"{user['login']}:{user['pwd']}")
+    except Exception as e:
+        return Verdict.DOWN("Can't connect to service", str(e))
+
+
+@Checker.define_get(vuln_num=2)
+def get_zip_flag(host: str, flag_id: str, flag: str) -> Verdict:
+    try:
+        with build_session() as session:
+            api = Api(host, session)
+
+            l, p = flag_id.split(':')
+            user = {'login': l, 'pwd': p}
+            resp = api.register_user(user)
+            if resp.status_code != 201:
+                return Verdict.MUMBLE("Can't login", "Can't login")
+
+            resp = api.get_notes(False)
+            if flag not in resp.text:
+                return Verdict.CORRUPT("Can't find flag", "Can't find flag from notes")
+
+            return Verdict.OK()
+    except Exception as e:
+        return Verdict.DOWN("Can't connect to service", str(e))
+
+
 if __name__ == '__main__':
     # Checker.run()
-    print(check_service("10.33.64.130:5000")._code)
+    service = check_service("10.33.64.130:8080")
+    print(service._code)
+    print(service._private_message)
