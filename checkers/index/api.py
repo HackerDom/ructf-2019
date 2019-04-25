48b72c23d5b21fe96b04badbf49f5b877ef2c04f
Alexey <a.dubrovin@skbkontur.ru>
2019-04-26 00:15:07 +0500

    fix port

--- checkers/index/api.py
+++ checkers/index/api.py
@@ -5,7 +5,7 @@ from utils.http import wait
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 
-PORT = ''  # TODO dont forget
+PORT = ':8888'
 
 
 class Api:
