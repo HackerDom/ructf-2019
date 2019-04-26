bf1b0470203ef8f3b581b7e16d304f526ee33e92
Alexey <a.dubrovin@skbkontur.ru>
2019-04-27 04:37:02 +0500

    upload styles

--- services/index/index/ClientApp/src/components/Upload.js
+++ services/index/index/ClientApp/src/components/Upload.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Alert, Button, Col, Container, Form, FormGroup, Input, Label, Spinner } from 'reactstrap';
+import { Alert, Button, Col, Form, FormGroup, Input, Label, Spinner } from 'reactstrap';
 
 export default class Upload extends React.Component {
     constructor(props) {
@@ -11,22 +11,28 @@ export default class Upload extends React.Component {
 
     submitForm(e) {
         e.preventDefault();
-        this.setState({ error: null, fetching: true, success: true });
+        if (!this.state.file) {
+            this.setState({ error: 'no file' });
+            return;
+        }
+        this.setState({ error: null, fetching: true, success: false });
         const form = new FormData();
         form.append('file', this.state.file);
-
         fetch('api/files', {
             method: 'POST',
             body: form
         }).then(async resp => {
-            if (!resp.ok)
+            if (!resp.ok) {
                 if (resp.status === 403)
                     this.setState({ error: "Unauthorized" });
-                else
+                else if (resp.status >= 500) {
+                    this.setState({ error: 'internal server error' });
+                } else
                     throw await resp.json();
-        }).then(_ => this.setState({
-            success: true
-        })).catch(json => this.setState({
+            } else {
+                this.setState({ success: true })
+            }
+        }).catch(json => this.setState({
             error: json.error
         })).finally(_ => this.setState({ fetching: false }));
     };
@@ -34,22 +40,20 @@ export default class Upload extends React.Component {
     handleChange = e => this.setState({ file: e.target.files[0] });
 
     render() {
-        return <Container>
-            <Col sm={3} md={{ size: 8, offset: 2 }}>
-                <Form onSubmit={this.submitForm} id="uploadForm">
-                    <FormGroup row>
-                        <Label for="file" sm={2}>Zip File</Label>
-                        <Col sm={7}>
-                            <Input type="file" name="file" id="file" onChange={this.handleChange} />
-                        </Col>
-                        <Button>
-                            {this.state.fetching && <Spinner size="sm" color="dark" />}
-                            upload</Button>
-                    </FormGroup>
-                </Form>
-                {this.state.error && <Col sm={5}><Alert color="danger">{this.state.error}</Alert></Col>}
-                {this.state.success && <Col sm={5}><Alert color="success">Uploaded</Alert></Col>}
-            </Col>
-        </Container>;
+        return <div className='upload-form'>
+            <Form onSubmit={this.submitForm} id="uploadForm">
+                <FormGroup row>
+                    <Label for="file" className='light-purple link'>
+                        {this.state.file ? `selected: ${this.state.file.name}` : 'select file'}
+                    </Label>
+                    <Col sm={4}>
+                        <Input className='upload-input' type="file" name="file" id="file" onChange={this.handleChange}/>
+                    </Col>
+                    <Button>{this.state.fetching && <Spinner size="sm" color="dark"/>} upload</Button>
+                </FormGroup>
+            </Form>
+            {this.state.error && <Alert color="danger">{this.state.error}</Alert>}
+            {this.state.success && <Alert color="success">Uploaded</Alert>}
+        </div>
     }
 }
