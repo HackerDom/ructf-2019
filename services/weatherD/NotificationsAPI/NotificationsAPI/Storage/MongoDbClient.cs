c6f6e7084e74e4193f972722690a6ad280e6cd37
Татьяна Гладышева <gladysheva@skbkontur.ru>
2019-04-28 03:27:57 +0500

    Доделки

--- services/weatherD/NotificationsAPI/NotificationsAPI/Storage/MongoDbClient.cs
+++ services/weatherD/NotificationsAPI/NotificationsAPI/Storage/MongoDbClient.cs
@@ -13,39 +13,40 @@ namespace NotificationsApi.Storage
 	    private const string MessagesDataDbName = "messages";
 
 		private readonly IMongoDatabase authorizesUsersDatabase;
-
-		public MongoDbClient(string connectionString)
+	    private IMongoCollection<SourceShortedData> sourecShortedDataCollection;
+	    private IMongoCollection<StoredMessage> storedMessageCollection;
+		public MongoDbClient(string connectionString, TimeSpan ttl)
         {
 	        var client = new MongoClient(connectionString);
 			authorizesUsersDatabase = client.GetDatabase(SourceDataDbName);
+			sourecShortedDataCollection = authorizesUsersDatabase.GetCollection<SourceShortedData>(SourceDataDbName);
+	        storedMessageCollection = authorizesUsersDatabase.GetCollection<StoredMessage>(MessagesDataDbName);
+			
+	        sourecShortedDataCollection.Indexes.CreateOne(Builders<SourceShortedData>.IndexKeys.Ascending("expireAt"), new CreateIndexOptions { ExpireAfter = ttl});
+	        storedMessageCollection.Indexes.CreateOne(Builders<StoredMessage>.IndexKeys.Ascending("expireAt"), new CreateIndexOptions { ExpireAfter = ttl });
+
 		}
 
-        public async Task InsertUser(string source, string token, string password, bool isPublic)
-        {
-            var collection = authorizesUsersDatabase.GetCollection<SourceShortedData>(SourceDataDbName);
-	        
+		public async Task InsertUser(string source, string token, string password, bool isPublic)
+        { 
 			var newData = new SourceShortedData(source, token, password, isPublic);
-            await collection.InsertOneAsync(newData);
+            await sourecShortedDataCollection.InsertOneAsync(newData);
         }
 
         public Task<List<SourceShortedData>> GetAllUsers()
         {
-            var collection = authorizesUsersDatabase.GetCollection<SourceShortedData>(SourceDataDbName);
-            return collection.Find(_ => true).ToListAsync();
+	        return sourecShortedDataCollection.Find(_ => true).ToListAsync();
         }
 
 	    public async Task InsertMessage(string source, Message message)
 	    {
-		    var collection = authorizesUsersDatabase.GetCollection<StoredMessage>(MessagesDataDbName);
-		    collection.Indexes.CreateOne(Builders<StoredMessage>.IndexKeys.Ascending("expireAt"), new CreateIndexOptions { ExpireAfter = new TimeSpan(0, 0, 10) });
 			var newData = new StoredMessage(source, message.Content, message.ExpireAt);
-		    await collection.InsertOneAsync(newData);
+		    await storedMessageCollection.InsertOneAsync(newData);
 	    }
 
 	    public Task<List<StoredMessage>> GetAllMessages()
 	    {
-		    var collection = authorizesUsersDatabase.GetCollection<StoredMessage>(MessagesDataDbName);
-		    return collection.Find(_ => true).ToListAsync();
+		    return storedMessageCollection.Find(_ => true).ToListAsync();
 		}
 	}
 }
